d9c003e2c2def714532249c5fd93548cfd8f0ced
Jim Carson <jim_carson@fastmail.com>
2021-11-30 21:35:50 -0800

    Day 1.

---
+++ day01.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("day01.txt", header=None)
+
+# part 1, count numbef of times it changes.
+print(np.sum(df.diff() > 0))
+
+
+# part 2, use a 3-number sample.  You can do this in one line, but it's
+# very dense.
+windows = df.rolling(3).sum()
+print(np.sum(windows.diff()> 0))
