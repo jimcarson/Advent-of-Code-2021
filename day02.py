4960f4997764d55903459b0dac472ac50d93e4b6
Jim Carson <jim_carson@fastmail.com>
2021-12-01 21:16:15 -0800

    Day 2 Advent of Code

---
+++ day02.py
@@ -0,0 +1,33 @@
+import pandas as pd
+df= pd.read_csv("day02.test.txt", sep=" ", header=None)
+df.columns = ["Direction","Magnitude"]
+h = 0
+v = 0
+aim = 0
+for i in df.index:
+    m = df.loc[i,"Magnitude"]
+    if df.loc[i,"Direction"] == "forward":
+        h += m
+    elif df.loc[i,"Direction"] == "down":
+        v += m
+    elif df.loc[i,"Direction"] == "up":
+        v -= m
+
+print(h,v,h*v)
+
+h = 0
+v = 0
+aim = 0
+for i in df.index:
+    m = df.loc[i,"Magnitude"]
+    if df.loc[i,"Direction"] == "forward":
+        h += m
+        v += aim * m
+    elif df.loc[i,"Direction"] == "down":
+        # v += m
+        aim += m
+    elif df.loc[i,"Direction"] == "up":
+        # v -= m
+        aim -= m
+
+print(h,v,h*v)
